4671889b3ef1593d4c11fd72098e145a92eaa7b9
Joe <jogo.522.522@gmail.com>
2015-04-18 19:26:57 -0400

    init

--- Assets/paddleMovement.cs
+++ Assets/paddleMovement.cs
@@ -1,8 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+using LockingPolicy = Thalmic.Myo.LockingPolicy;
+using Pose = Thalmic.Myo.Pose;
+using UnlockType = Thalmic.Myo.UnlockType;
+using VibrationType = Thalmic.Myo.VibrationType;
+
 public class paddleMovement : MonoBehaviour {
 	Vector3 offset;
+	public GameObject myo = null;
 	// Use this for initialization
 	void Start () {
 		offset= new Vector3((Input.mousePosition.x - 0) * (10) / Screen.width,Screen.height/2-(Input.mousePosition.y - 0) * (10) / Screen.height + -5,0);
@@ -10,12 +16,13 @@ public class paddleMovement : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		float x = (Input.mousePosition.x - 0) * (7.2f) / Screen.width + -3;//-offset.x;//subtract a1, multiply range2/range1, add a2
-		float y = (Input.mousePosition.y - 0) * (2.8f) / Screen.height ;//-offset.y;
+		ThalmicMyo thalmicMyo = myo.GetComponent<ThalmicMyo> ();
+		float x = (thalmicMyo.accelerometer.x - 0) * (7.2f) / Screen.width + -3;//-offset.x;//subtract a1, multiply range2/range1, add a2
+		float y = (thalmicMyo.accelerometer.y - 0) * (2.8f) / Screen.height ;//-offset.y;
 		this.transform.position = new Vector3(x,y,0);
-		if (Input.GetKey(KeyCode.A))
+		if (thalmicMyo.pose == Pose.WaveIn) 
 				rotateLeft ();
-		if (Input.GetKey(KeyCode.D))
+		if (thalmicMyo.pose == Pose.WaveOut)
 			rotateRight ();
 	}
 
